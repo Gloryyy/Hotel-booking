d1ccf239aac10485a87cf81808b59ee30db9eb88
gloryyy <majdwarrrak@gmail.com>
2021-07-30 23:22:05 +0200

    adding Register form component and creating our API to connect to database by using axios

--- src/App.js
+++ src/App.js
@@ -3,11 +3,14 @@ import Home from "./booking/Home";
 import Login from "./auth/Login";
 import Register from "./auth/Register";
 import TopNav from "./components/TopNav";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 function App() {
   return (
     <Router>
       <TopNav />
+      <ToastContainer position="top-center" autoClose={2000} />
       <Switch>
         <Route exact path="/" component={Home} />
         <Route path="/login" component={Login} />
